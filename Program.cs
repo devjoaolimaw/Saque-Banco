6473f688eac620ee0b322d729ac8f8483369b7c1
João Paulo <167019869+devjoaolimaw@users.noreply.github.com>
2024-05-30 21:20:35 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int saldoTotal = 1500;
+        Console.WriteLine($"Você possui R${saldoTotal},00");
+        
+        Console.WriteLine();
+
+        Console.WriteLine("Quanto deseja diretirar?");
+        int valorSaque = int.Parse(Console.ReadLine());
+        
+        int saldo = saldoTotal - valorSaque;
+        
+        Console.WriteLine("Por fovor aguarde...");
+        Thread.Sleep(2000);
+
+        if (valorSaque <= saldoTotal)
+        {
+          Console.WriteLine($"Saque realizado com sucesso! Novo saldo: {saldo}");
+        }
+        else
+        {
+          Console.WriteLine("Saldo insuficiente. Saque nao realizado!");
+        }
+    }
+}
